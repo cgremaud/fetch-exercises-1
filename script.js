35c41267443c45657d1522ed33de52cf41b7ac73
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-11 09:53:47 -0600

    added planet select button

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 window.addEventListener("load", (event) => {
     // TODO: fetch planets JSON
     console.log(event)
-    
-    let fetchPromise = fetch("https://handlers.education.launchcode.org/static/planets.json")
-    let destinationElement = document.getElementById("destination")
+
+    const fetchPromise = fetch("https://handlers.education.launchcode.org/static/planets.json")
+    const destinationElement = document.getElementById("destination")
+    const planetSelect = document.getElementById("planet-select")
 
     fetchPromise.then((response) => {
-        let jsonPromise = response.json()
+        const jsonPromise = response.json()
 
         jsonPromise.then((json) => {
-            console.log(json)
-            destinationElement.innerHTML = `<h3> Planet: ${json[0].name}</h3>`
-        })
-    })
+            console.log(json);
+            planetSelect.addEventListener("click", (event) => {
+                destinationElement.innerHTML += `<h3> 
+                    Planet: ${json[1].name} <br />
+                    Diameter: ${json[1].diameter} <br />
+                    </h3><br />
+                    <img height = "200px" src = ${json[1].image}>`;
+                });
+        });
+    });
  });
